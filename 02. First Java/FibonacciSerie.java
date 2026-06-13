46ad870cf74417f6906c57a5e36ccc1b816e508b
Amit <vraiamit@gmail.com>
2026-06-13 22:08:03 +0530

    day 2

--- 02. First Java/FibonacciSerie.java
+++ 02. First Java/FibonacciSerie.java
@@ -1,1 +1,26 @@
-// To calculate Fibonacci Series up to n numbers.
+import java.util.*;
+
+class FibonacciSerie {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter numbers to print: ");
+        int n = sc.nextInt();
+        
+        int a = 0;
+        int b = 1;
+        int next;
+        
+        System.out.println("Fibonacci Series is : ");
+        
+        for (int x = 1; x <= n; x++) {
+            System.out.print(a + " "); // a ko print kiya aur space add kiya
+            
+            // Yahan se int hata diya gaya hai kyunki variables upar ban chuke hain
+            next = a + b; 
+            a = b;        
+            b = next;     
+        }
+        
+        sc.close(); // Scanner ko close karna ek good practice hai
+    }
+}
