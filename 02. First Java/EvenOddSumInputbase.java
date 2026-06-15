f93f1de3c46a199c6610baec8ba7c1831431f3f9
Amit <vraiamit@gmail.com>
2026-06-15 06:46:48 +0530

    day 3

---
+++ 02. First Java/EvenOddSumInputbase.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class EvenOddSumInputbase{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("How much number you want to put :");
+        int n = sc.nextInt();
+        int sum_odd =0 , sum_even = 0 ;  // java me jb bhi koi variable iniialise ktro hmesha value de do 
+        while(n>0){
+            System.out.println("Enter number :");
+            int i = sc.nextInt();
+            if (i%2==0){
+                sum_even= sum_even+i;
+            }
+            else{
+                sum_odd=sum_odd+i;
+
+            }
+            n--;
+
+        }
+        System.out.println("Sum of even number  :" + (sum_even));
+        System.out.println("Sum of odd number :" +(sum_odd));
+
+
+        
+    }
+    
+}
---
+++ 02. First Java/Multiplation.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Multiplation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Input n :");
+        int n = sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println((n)+"*"+(i)+"="+(i*n));
+        }
+
+
+    }
+}
